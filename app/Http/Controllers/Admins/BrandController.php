45a0563f9ca2e6b91f05cfb486ceb150a21cf740
Espinado <roman.vyshedko@inbox.lv>
2022-05-01 01:48:59 +0300

    brand data save

--- app/Http/Controllers/Admins/BrandController.php
+++ app/Http/Controllers/Admins/BrandController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admins;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Image;
+use App\Models\Admins\Brand;
 
 class BrandController extends Controller
 {
@@ -15,11 +17,22 @@ class BrandController extends Controller
 
     public function index()
     {
-        return view('admin.brands.index');
+        $brands=Brand::all();
+        return view('admin.brands.index', compact ('brands'));
     }
 
     public function store(Request $request) {
-        dd($request->all());
+
+        $originalImage= $request->file('brand_logo');
+        $brand_logo = Image::make($originalImage);
+        $path = public_path() . '/brands/';
+        $brand_logo->resize(150, 150);
+        $brand_logo->save($path . time() . $originalImage->getClientOriginalName());
+        $brand= new Brand();
+        $brand->brand_logo = time() . $originalImage->getClientOriginalName();
+        $brand->brand_name = $request->brand_name;
+        $brand->save();
+        return back()->with('success', 'Recorded');
 
     }
 }
