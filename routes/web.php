124ccd4dfc8e7a5a8d89077fae09010d3cfd4ea3
Espinado <roman.vyshedko@inbox.lv>
2023-01-26 13:13:51 +0200

    tag details

--- routes/web.php
+++ routes/web.php
@@ -142,6 +142,7 @@ Route::group(
         Route::get('/password/change', [ProfileController::class, 'changePassword'])->name('user.change.password');
         Route::post('/password/update', [ProfileController::class, 'updatePassword'])->name('user.update.password');
         Route::get('/product/details/{id}/{slug}', [IndexController::class, 'productDetails'])->name('product.details');
+        Route::get('/product/tag/{product_tag}', [IndexController::class, 'productTag'])->name('product.tag');
         Route::get('add_wishlist/{id}', [CartController::class, 'add_wishlist'])->name('add_wishlist');
         require __DIR__ . '/auth.php';
     }
