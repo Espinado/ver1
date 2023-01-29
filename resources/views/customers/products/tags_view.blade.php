ae5124e3031ed2e1ac1ca968136b82cdb9712128
Espinado <roman.vyshedko@inbox.lv>
2023-01-29 22:49:51 +0200

    colors size to choose

--- resources/views/customers/products/tags_view.blade.php
+++ resources/views/customers/products/tags_view.blade.php
@@ -121,22 +121,7 @@
                         <!-- /.sidebar-widget -->
                         <!-- ============================================== MANUFACTURES: END ============================================== -->
                         <!-- ============================================== COLOR============================================== -->
-                        <div class="sidebar-widget wow fadeInUp">
-                            <div class="widget-header">
-                                <h4 class="widget-title">Colors</h4>
-                            </div>
-                            <div class="sidebar-widget-body">
-                                <ul class="list">
-                                    <li><a href="#">Red</a></li>
-                                    <li><a href="#">Blue</a></li>
-                                    <li><a href="#">Yellow</a></li>
-                                    <li><a href="#">Pink</a></li>
-                                    <li><a href="#">Brown</a></li>
-                                    <li><a href="#">Teal</a></li>
-                                </ul>
-                            </div>
-                            <!-- /.sidebar-widget-body -->
-                        </div>
+{{-- @include ('customers.sections.products.color_list') --}}
                         <!-- /.sidebar-widget -->
                         <!-- ============================================== COLOR: END ============================================== -->
                         <!-- ============================================== COMPARE============================================== -->
