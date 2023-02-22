24e1e3af7ff9fc3fd0789acf408c6017014a0747
Espinado <roman.vyshedko@inbox.lv>
2023-02-22 20:06:36 +0200

    bug fix in checkouts

--- resources/views/customers/sections/shopping_cart_dropdown.blade.php
+++ resources/views/customers/sections/shopping_cart_dropdown.blade.php
@@ -19,7 +19,7 @@
               <div class="pull-right"> <span class="text">Sub Total :</span>
                     <span class='price'  id="cartSubTotal"> </span> </div>
                  <div class="clearfix"></div>
-                 <a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a>
+                 <a href="{{route('product.checkout')}}" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a>
              </div>
              <!-- /.cart-total-->
 
